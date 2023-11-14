5b31dacf51818e2150064b1469ae560718cee541
Bowman <mb6122e@gre.ac.uk>
2023-11-14 09:56:13 +0000

    sda

---
+++ Unity Games/Custom Editor Scripts Project/Scripts/Editor/RocketJump/Scripts/PortfolioWorksGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PortfolioWorksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTFOLIOWORKS_API APortfolioWorksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
